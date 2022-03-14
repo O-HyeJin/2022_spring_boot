d04687036a9ced029263817ca3fb24fe6328edc0
O-HyeJin <ssin31400@gmail.com>
2022-03-14 13:59:49 +0900

    Feat: sqlYearStatistic URL 매핑으로 연별 로그인 수 확인하는 메소드 작성

--- src/main/java/com/comento/spring_boot/test/Test.java
+++ src/main/java/com/comento/spring_boot/test/Test.java
@@ -1,13 +1,28 @@
 package com.comento.spring_boot.test;
 import java.util.ArrayList;
 import java.util.List;
- 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.comento.spring_boot.service.StatisticService;
  
 @Controller
 public class Test {
+	
+	@Autowired
+	private StatisticService service;
+	
+	@ResponseBody
+	@RequestMapping("/sqlYearStatistic")
+	public Map<String, Object> sqlTest(String year) throws Exception {
+		return service.yearLoginNum(year);
+	}
+	
 	@RequestMapping("/test") 
     public ModelAndView test() throws Exception{ 
         ModelAndView mav = new ModelAndView("test"); 
