2264329f0885801a3584d4d37c075b217eece939
O-HyeJin <ssin31400@gmail.com>
2022-03-16 20:13:35 +0900

    Chore: 패키지명 수정 및 mapper 위치 설정 추가

--- src/main/java/com/comento/spring_boot/config/MybatisConfig.java
+++ src/main/java/com/comento/spring_boot/config/MybatisConfig.java
@@ -8,9 +8,10 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
-@MapperScan(basePackages = "com.comento.spring_boot.dao")
+@MapperScan(basePackages = "com.comento.spring_boot.mapper")
 public class MybatisConfig {
 	
 	@Bean
@@ -19,12 +20,18 @@ public class MybatisConfig {
 		
 		sqlSessionFactory.setDataSource(dataSource);
 		sqlSessionFactory.setTypeAliasesPackage("com.comento.spring_boot.dto");
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		
+		sqlSessionFactory.setConfigLocation(resolver.getResource("classpath:mybatis/mybatis-config.xml"));
+		sqlSessionFactory.setMapperLocations(resolver.getResources("classpath:mybatis/mapper/*.xml"));
 		
 		return sqlSessionFactory.getObject();
 	}
 	
 	@Bean
 	public SqlSessionTemplate sqlSession (SqlSessionFactory sqlSessionFactory) {
+		sqlSessionFactory.getConfiguration().setMapUnderscoreToCamelCase(true);
 		return new SqlSessionTemplate(sqlSessionFactory);
 	}
+	
 }
