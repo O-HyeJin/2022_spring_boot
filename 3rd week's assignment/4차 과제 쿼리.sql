ebee904a78e85d92734dec1ccb47d4ce9efe4630
O-HyeJin <ssin31400@gmail.com>
2022-03-18 15:49:28 +0900

    Chore: 평균 하루 로그인 수, 휴일을 제외한 로그인 수 where 조건 변경
    - 월별로 구분 가능하게 함

--- 3rd week's assignment/4차 과제 쿼리.sql
+++ 3rd week's assignment/4차 과제 쿼리.sql
@@ -22,27 +22,28 @@ SELECT COUNT(*) AS totCnt, '200821' AS date
 	yearMonth = 2008, organ = A */
 SELECT COUNT(*) AS totCnt, '2008' AS date, u.organization
 	FROM statistic.requestinfo ri, statistic.user u
-	WHERE (left(ri.createDate, 4) = '2008') AND ri.userID = u.userID
-	GROUP BY u.organization;
+	WHERE (left(ri.createDate, 4) = '2008') AND (ri.userID = u.userID)
+	AND (u.organization = "A");
 	
 /* 평균 하루 로그인 수 
-	average = true */
+	yearMonth = 2008, average = true */
 SELECT AVG(reqCount.loginCnt) as totCnt
 	from 
 		(SELECT COUNT(*) AS loginCnt, LEFT(ri.createDate, 6) AS date 
 		FROM statistic.requestinfo ri
-		WHERE ri.requestCode = "L" GROUP BY date) reqCount;
+		WHERE ri.requestCode = "L" GROUP BY date) reqCount
+	WHERE (left(date, 4) = '2008');
 
 /* 휴일을 제외한 로그인 수(여기서는 주말 포함)
 	따라서 3차 과제에서는 총 로그인 수를 구한다.
-	weekday = true */
+	yearMonth = 2008, weekday = true */
 SELECT COUNT(*) AS totCnt
 	FROM statistic.requestinfo ri
-	WHERE ri.requestCode = "L";
+	WHERE (left(ri.createDate, 4) = '2008') AND (ri.requestCode = "L");
 
 
 
 /* 일자별 로그인 수(평일 하루 로그인 수 서브쿼리) */
 SELECT COUNT(*) AS loginCnt, LEFT(ri.createDate, 6) AS date 
 	FROM statistic.requestinfo ri
-	WHERE ri.requestCode = "L" GROUP BY date;
+	WHERE requestCode = "L" GROUP BY date;
