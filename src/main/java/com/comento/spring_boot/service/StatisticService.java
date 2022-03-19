1035fca9157512be5c5d9106b4c87ea48fb74138
O-HyeJin <ssin31400@gmail.com>
2022-03-19 18:48:26 +0900

    Feat: 월별, 일별, 부서별 접속자 수, 평균 하루 로그인 수를 구하는 서비스 내 메소드 작성
    - setResultMap > 각 DTO 리스트를 Map 형식으로 바꿔주는 메소드

--- src/main/java/com/comento/spring_boot/service/StatisticService.java
+++ src/main/java/com/comento/spring_boot/service/StatisticService.java
@@ -4,4 +4,9 @@ import java.util.HashMap;
 
 public interface StatisticService {
 	public HashMap<String, Object> yearLoginNum (String year);
+	public HashMap<String, Object> monthConnNum();
+	public HashMap<String, Object> dayConnNum();
+	public HashMap<String, Object> organConnNum();
+	public HashMap<String, Object> avgLoginNum();
+	public HashMap<String, Object> weekdayLoginNum();
 }
--- src/main/java/com/comento/spring_boot/service/StatisticServiceImpl.java
+++ src/main/java/com/comento/spring_boot/service/StatisticServiceImpl.java
@@ -2,31 +2,38 @@ package com.comento.spring_boot.service;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.comento.spring_boot.dto.GroupByStatisticDto;
+import com.comento.spring_boot.dto.AvgStatisticDto;
+import com.comento.spring_boot.dto.OrganStatisticDto;
+import com.comento.spring_boot.dto.StatisticDto;
+import com.comento.spring_boot.dto.StatisticObj;
+import com.comento.spring_boot.dto.TestDto;
 import com.comento.spring_boot.mapper.StatisticMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
+@SuppressWarnings("unchecked")
 public class StatisticServiceImpl implements StatisticService{
 	
-	@Autowired
 	private StatisticMapper mapper;
+	private ObjectMapper objMapper;
 	
-	@SuppressWarnings("unchecked")
+	// ----------test----------
 	@Override
 	public HashMap<String, Object> yearLoginNum(String year) {
 		LinkedHashMap<String, Object> result = new LinkedHashMap<String, Object>();
 		
 		try {
-			GroupByStatisticDto dto = mapper.selectYearLogin(year);
+			TestDto dto = mapper.selectYearLogin(year);
 			dto.setDate("20"+dto.getDate());
 			result.put("is_success", true);
 
-			ObjectMapper objMapper = new ObjectMapper(); 
 			String dataString = objMapper.writeValueAsString(dto);
 			
 			LinkedHashMap<String, Object> dataMap = objMapper.readValue(dataString, LinkedHashMap.class);
@@ -40,5 +47,56 @@ public class StatisticServiceImpl implements StatisticService{
 
 		return result;
 	}
+	
+	
+	// ----------SW 활용 현황 API----------
+	@Override
+	public HashMap<String, Object> monthConnNum() {
+		List<StatisticDto> list = mapper.selectGroupByMonth();
+		return setResultMap(list);
+	}
+
+	@Override
+	public HashMap<String, Object> dayConnNum() {
+		List<StatisticDto> list = mapper.selectGroupByDay();
+		return setResultMap(list);
+	}
+
+	@Override
+	public HashMap<String, Object> organConnNum() {
+		List<OrganStatisticDto> list = mapper.selectGroupByOrgan();
+		return setResultMap(list);
+	}
+
+	@Override
+	public HashMap<String, Object> avgLoginNum() {
+		List<AvgStatisticDto> list = mapper.selectAvgGroupByMonth();
+		return setResultMap(list);
+	}
+
+	@Override
+	public HashMap<String, Object> weekdayLoginNum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public LinkedHashMap<String, Object> setResultMap(List<? extends StatisticObj> list) {
+		LinkedHashMap<String, Object> result = new LinkedHashMap<String, Object>();
+		try {
+			result.put("is_success", true);
+			
+			for (int i=0;i<list.size();i++) {
+				String dataString = objMapper.writeValueAsString(list.get(i));
+				LinkedHashMap<String, Object> dataMap = objMapper.readValue(dataString, LinkedHashMap.class);
+				result.put("no"+(i+1), dataMap);
+			}
+		} catch (Exception e) {
+			result.put("totCnt", -999);
+			result.put("is_success", false);
+		}
+		
+		return result;
+	}
+
 
 }
