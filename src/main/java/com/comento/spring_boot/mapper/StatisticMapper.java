8233ae6055413130b6fafc1ddf1f853688026539
O-HyeJin <ssin31400@gmail.com>
2022-03-19 18:43:22 +0900

    Feat: 월별 접속자 수, 일별 접속자 수, 부서별 접속자 수, 평균 하루 로그인 수를 구하는 쿼리 작성 및 mapper 설정

--- src/main/java/com/comento/spring_boot/mapper/StatisticMapper.java
+++ src/main/java/com/comento/spring_boot/mapper/StatisticMapper.java
@@ -1,12 +1,24 @@
 package com.comento.spring_boot.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import com.comento.spring_boot.dto.GroupByStatisticDto;
+import com.comento.spring_boot.dto.AvgStatisticDto;
+import com.comento.spring_boot.dto.OrganStatisticDto;
+import com.comento.spring_boot.dto.StatisticDto;
+import com.comento.spring_boot.dto.TestDto;
 
 @Repository
 @Mapper
 public interface StatisticMapper {
-	public GroupByStatisticDto selectYearLogin(String year); 
+	// 테스트용
+	public TestDto selectYearLogin(String year);
+	
+	// SW 활용 현황 API
+	public List<StatisticDto> selectGroupByMonth();
+	public List<StatisticDto> selectGroupByDay();
+	public List<OrganStatisticDto> selectGroupByOrgan();
+	public List<AvgStatisticDto> selectAvgGroupByMonth();
 }
