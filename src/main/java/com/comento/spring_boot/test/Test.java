962702e5f63f201c95490113381d80f7fc72dfe2
O-HyeJin <ssin31400@gmail.com>
2022-03-20 22:55:13 +0900

    Feat: 외부 API 사용 방법 테스트
    - 외부 API의 XML 결과를 JSON 형태로 가져오기
    - pom.xml의 추가 라이브러리들은 추후 안 쓰는 것들 정리할 것

--- src/main/java/com/comento/spring_boot/test/Test.java
+++ src/main/java/com/comento/spring_boot/test/Test.java
@@ -1,8 +1,16 @@
 package com.comento.spring_boot.test;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +24,7 @@ public class Test {
 	
 	@Autowired
 	private StatisticService service;
+	public static int INDENT_FACTOR = 4;
 	
 	@ResponseBody
 	@RequestMapping("/sqlYearStatistic")
@@ -36,4 +45,35 @@ public class Test {
         mav.addObject("list", resultList); 
         return mav; 
     }
+	
+	@ResponseBody
+	@RequestMapping("/apiTest")
+	public String apiTest() throws Exception {
+		String urlstr = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?"
+				+ "solYear=2019"
+				+ "&solMonth=03"
+				+ "&ServiceKey=PHhqZKbAqKUUjsg25R1UiCGgTBFZtkJ7TNML%2BgBjZXf6i0KcscSuLZEB%2Fku3Zp%2FYJ2CS2vkLoOY%2BoE3R%2B3OESw%3D%3D";
+		URL url = new URL(urlstr);
+		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		urlConnection.connect();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+		StringBuffer st = new StringBuffer();
+        String line;
+        while ((line = br.readLine()) != null) {
+            st.append(line);
+        }
+        
+        JSONObject xmlJSONObj = XML.toJSONObject(st.toString());
+        
+        //String jsonPrettyPrintString = xmlJSONObj.toString(INDENT_FACTOR);
+	
+		JSONObject response = (JSONObject) xmlJSONObj.get("response");
+		JSONObject body = (JSONObject) response.get("body");
+		JSONObject items = (JSONObject) body.get("items");
+		
+		//System.out.println(jsonPrettyPrintString);
+		return items.toString(INDENT_FACTOR);
+		
+	}
 }
