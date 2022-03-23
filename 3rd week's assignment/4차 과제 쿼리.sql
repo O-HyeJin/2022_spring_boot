6f1badef124618c810b589fb6e4aa408ebe34a45
O-HyeJin <ssin31400@gmail.com>
2022-03-23 15:21:14 +0900

    Feat: 휴일 제외한 로그인 수 API 결과에서 주말도 제외
    - 쿼리에서 dayofweek 함수 사용

--- 3rd week's assignment/4차 과제 쿼리.sql
+++ 3rd week's assignment/4차 과제 쿼리.sql
@@ -8,28 +8,28 @@ SELECT DISTINCT count(*) as totCnt, '20' as date, ri.requestCode
 		
 /* 월별 접속자 수(중복X)
 	groupByMonth */
-SELECT COUNT(*) AS totCnt, LEFT(ri.createDate, 4) AS date
+SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 4)) AS date
 	FROM statistic.requestinfo ri
 	GROUP BY date;
 
 /* 일별 접속자 수(중복X)
 	groupByDay */
-SELECT COUNT(*) AS totCnt, LEFT(ri.createDate, 6) AS date
+SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 6)) AS date
 	FROM statistic.requestinfo ri
 	GROUP BY date;
 
 /* 부서별(월별) 접속자 수(중복X)
 	groupByOrgan */
-SELECT COUNT(*) AS totCnt, LEFT(ri.createDate, 4) AS date, u.organization AS organ
+SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 4)) AS date, u.organization AS organ
 	FROM statistic.requestinfo ri, statistic.user u
 	WHERE ri.userID = u.userID
 	GROUP BY u.organization, date; 
 	
 /* 평균 하루 로그인 수 
 	groupByMonth, average */
-SELECT AVG(reqCount.loginCnt) as totCnt, LEFT(sub_date, 4) AS date
+SELECT AVG(reqCount.loginCnt) as totCnt, LEFT(sub_date, 6) AS date
 	from 
-		(SELECT COUNT(*) AS loginCnt, LEFT(ri.createDate, 6) AS sub_date
+		(SELECT COUNT(*) AS loginCnt, CONCAT("20", LEFT(ri.createDate, 6)) AS sub_date
 		FROM statistic.requestinfo ri
 		WHERE ri.requestCode = "L" GROUP BY sub_date) reqCount
 	GROUP BY date;
@@ -37,14 +37,22 @@ SELECT AVG(reqCount.loginCnt) as totCnt, LEFT(sub_date, 4) AS date
 /* 휴일을 제외한 로그인 수(여기서는 주말 포함)
 	따라서 3차 과제에서는 총 로그인 수를 구한다.
 	groupByDay, weekday */
+	
+-- 일별 평일 로그인 수
 SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 6))  AS date 
 	FROM statistic.requestinfo ri
-	WHERE requestCode = "L" GROUP BY date;
+	WHERE requestCode = "L" AND isHoliday(ri.createDate)
+	GROUP BY date;
+	
+-- isHoliday > return DAYOFWEEK(STR_TO_DATE(LEFT(ri.createDate, 6),'%y%m%d')) NOT IN ('1', '7')
 
--- 월별 로그인 수 
-SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 4))  AS date 
-	FROM statistic.requestinfo ri
-	WHERE requestCode = "L" GROUP BY date;
+-- 월별 평일 로그인 수 
+SELECT COUNT(*) AS totCnt, LEFT(sub_date, 6) AS date 
+	FROM (SELECT COUNT(*) AS totCnt, CONCAT("20", LEFT(ri.createDate, 6)) AS sub_date 
+		FROM statistic.requestinfo ri
+		WHERE requestCode = "L" AND isHoliday(ri.createDate)
+		GROUP BY sub_date) reqDays
+	GROUP BY date;
 
 
 
