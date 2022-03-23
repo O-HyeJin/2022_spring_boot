bd2ef33940702757d1f2787ad9a71d499b844c85
O-HyeJin <ssin31400@gmail.com>
2022-03-23 20:21:33 +0900

    Chore: Import한 패키지 정리

--- src/main/java/com/comento/spring_boot/test/Test.java
+++ src/main/java/com/comento/spring_boot/test/Test.java
@@ -7,10 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
